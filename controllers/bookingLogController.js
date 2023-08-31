b8918dedf18d2378b27f95b4f8f40f8c30e5c46f
supreetkaur <supreet97@gmail.com>
2023-08-31 14:07:16 -0700

    logic for getting InProgress tasks

--- controllers/bookingLogController.js
+++ controllers/bookingLogController.js
@@ -1,9 +1,37 @@
 const knex = require("knex")(require("../knexfile"));
 
 const getInProgressTasks = (_req, res) => {
-  knex("user_request")
-    .where("user_id", 1)
-    .where("status", "In Progress")
+  //   knex("user_request")
+  //     .where("user_id", 1)
+  //     .where("status", "In Progress")
+  //     .then((data) => {
+  //       res.json(data);
+  //     })
+  //     .catch((error) => {
+  //       res.send(error);
+  //     });
+
+  knex
+    .select(
+      "category_name",
+      "category_description",
+      "firstname",
+      knex.raw("CONCAT(firstname, ' ', lastname) AS service_provider_name"),
+      "quote.user_request_id",
+      "quote.amount",
+      "quote.estimated_duration",
+      "quote.description"
+    )
+    .from("quote")
+    .join("user_request", "user_request_id", "user_request.id")
+    .join("service_category", "service_category_id", "service_category.id")
+    .join(
+      "service_provider",
+      "quote.service_provider_id",
+      "service_provider.id"
+    )
+    .where("user_request.status", "In Progress")
+    .orderBy("quote.id")
     .then((data) => {
       res.json(data);
     })
