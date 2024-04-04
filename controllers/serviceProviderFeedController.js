516234d3bb90437a9fc443090795714643ae885a
supreetkaur <supreet97@gmail.com>
2024-04-04 14:38:09 -0700

    added temporary kirtanData.csv for testing

--- controllers/serviceProviderFeedController.js
+++ controllers/serviceProviderFeedController.js
@@ -1,5 +1,22 @@
 const knex = require("knex")(require("../knexfile.js"));
 
+// const getAllRequests = (_req, res) => {
+//   knex("user_request")
+//     .join(
+//       "service_category",
+//       "user_request.service_category_id",
+//       "service_category.id"
+//     )
+//     .join("service", "service.service_category_id", "service_category.id")
+//     .where("status", "In Progress")
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.status(400).send("Error getting requests");
+//     });
+// };
+
 const getAllRequests = (_req, res) => {
   knex("user_request")
     .where("status", "In Progress")
